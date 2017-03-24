045a0b635a9737633207503eca1e622e5b7b1ecf
Nikolaus Schlemm <capo@coder-nostra.de>
2017-03-24 17:47:46 +0100

    let boto3 handle the credentials, etc.

--- dj_elastictranscoder/transcoder.py
+++ dj_elastictranscoder/transcoder.py
@@ -1,6 +1,5 @@
-from boto3.session import Session
+import boto3
 
-from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 
 from .models import EncodeJob
@@ -11,33 +10,8 @@ class Transcoder(object):
     def __init__(self, pipeline_id, region=None, access_key_id=None, secret_access_key=None):
         self.pipeline_id = pipeline_id
 
-        if not region:
-            region = getattr(settings, 'AWS_REGION', None)
-        self.aws_region = region
-
-        if not access_key_id:
-            access_key_id = getattr(settings, 'AWS_ACCESS_KEY_ID', None)
-        self.aws_access_key_id = access_key_id
-
-        if not secret_access_key:
-            secret_access_key = getattr(settings, 'AWS_SECRET_ACCESS_KEY', None)
-        self.aws_secret_access_key = secret_access_key
-
-        if self.aws_access_key_id is None:
-            assert False, 'Please provide AWS_ACCESS_KEY_ID'
-
-        if self.aws_secret_access_key is None:
-            assert False, 'Please provide AWS_SECRET_ACCESS_KEY'
-
-        if self.aws_region is None:
-            assert False, 'Please provide AWS_REGION'
-
-        boto_session = Session(
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
-            region_name=self.aws_region,
-        )
-        self.client = boto_session.client('elastictranscoder')
+        self.client = boto3.client('elastictranscoder', aws_access_key_id=access_key_id,
+                                   aws_secret_access_key=secret_access_key, region_name=region)
 
     def encode(self, input_name, outputs, **kwargs):
         self.message = self.client.create_job(
